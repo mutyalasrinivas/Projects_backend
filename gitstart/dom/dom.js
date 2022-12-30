6298978638391d0df79bf9293bf7c542d3d6cb77
mutyalasrinivas <srinivasmutyala54@gmail.com>
2022-12-30 15:39:58 +0530

    commit dom files

---
+++ gitstart/dom/dom.js
@@ -0,0 +1,15 @@
+// console.log(document.links)
+// console.log(document.images)
+// console.log(document.all)
+console.log(document.title)
+document.title="sharpener"
+console.log(document.title)
+var headerTitle=document.getElementById("header-title")
+// headerTitle.textContent="good"
+// headerTitle.innerText="morning"
+console.log(headerTitle)
+var header=document.getElementById('main-header')
+header.style.borderBottom='solid 3px #000'
+var subHead=document.getElementById('sub-head')
+subHead.style.color='green'
+subHead.style.fontWeight='bold'
