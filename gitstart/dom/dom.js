7ef5f2313c2f680d91f67171e9747c4c7fefe411
mutyalasrinivas <srinivasmutyala54@gmail.com>
2022-12-30 16:43:31 +0530

    getbyTag commited

--- gitstart/dom/dom.js
+++ gitstart/dom/dom.js
@@ -21,3 +21,13 @@ for(var i=0;i<items.length;i++){
     items[i].style.color='yellow'
     items[i].style.fontWeight='bold'
 }
+var li=document.getElementsByTagName('li')
+console.log(li)
+li[2].style.backgroundColor='green'
+for(var i=0;i<li.length;i++){
+    li[i].style.color='yellow'
+    li[i].style.fontWeight='bold'
+}
+
+li[1].style.color='red'
+ 
