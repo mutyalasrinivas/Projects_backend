9aab94e6a59075a6a1685c2ce7391a0cfd172d40
mutyalasrinivas <srinivasmutyala54@gmail.com>
2022-12-30 16:21:17 +0530

    getByClass commited

--- gitstart/dom/dom.js
+++ gitstart/dom/dom.js
@@ -8,8 +8,16 @@ var headerTitle=document.getElementById("header-title")
 // headerTitle.textContent="good"
 // headerTitle.innerText="morning"
 console.log(headerTitle)
-var header=document.getElementById('main-header')
-header.style.borderBottom='solid 3px #000'
-var subHead=document.getElementById('sub-head')
-subHead.style.color='green'
-subHead.style.fontWeight='bold'
+// var header=document.getElementById('main-header')
+// header.style.borderBottom='solid 3px #000'
+// var subHead=document.getElementById('sub-head')
+// subHead.style.color='green'
+// subHead.style.fontWeight='bold'
+
+var items=document.getElementsByClassName('list-group-item')
+console.log(items)
+items[2].style.backgroundColor='green'
+for(var i=0;i<items.length;i++){
+    items[i].style.color='yellow'
+    items[i].style.fontWeight='bold'
+}
