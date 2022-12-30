8c0fdb6bbb41067f257d271501a390a4625fe871
mutyalasrinivas <srinivasmutyala54@gmail.com>
2022-12-30 21:23:00 +0530

    dom manipulation commited

--- dom/dom.js
+++ dom/dom.js
@@ -41,10 +41,93 @@ console.log(document.title)
 // thirdItem.style.visibility='hidden'
 
 //querySelectorAll
-var secondItem=document.querySelectorAll('.list-group-item:nth-child(2)')
-console.log(secondItem)
-//  secondItem.style.backgroundColor='yellow'
-var odd=document.querySelectorAll('.list-group-item:nth-child(odd)')
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor='green'
-}
+// var secondItem=document.querySelectorAll('.list-group-item:nth-child(2)')
+// console.log(secondItem)
+// //  secondItem.style.backgroundColor='yellow'
+// var odd=document.querySelectorAll('.list-group-item:nth-child(odd)')
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor='green'
+// }
+
+//  var itemList=document.querySelector('#items');
+// //  parent node
+//  console.log(itemList.parentNode);
+//   itemList.parentNode.style.backgroundColor='#f4f4f4';
+//   console.log(itemList.parentNode.parentNode.parentNode)
+//  parent Element
+//   console.log(itemList.parentElement);
+//    itemList.parentElement.style.backgroundColor='#f4f4f4';
+//    console.log(itemList.parentElement.parentElement.parentElement)
+
+//childNodes
+// console.log(itemList.childNodes)
+// console.log(itemList.children);
+// console.log(itemList.children[1])
+// itemList.children[1].style.backgroundColor='yellow'
+
+// //firstChild
+// console.log(itemList.firstChild)
+// //firstElementChild
+// console.log(itemList.firstElementChild)
+// itemList.firstElementChild.textContent='first'
+
+// //lastChild
+// console.log(itemList.lastChild)
+// //lastElementChild
+// console.log(itemList.lastElementChild)
+// itemList.lastElementChild.textContent='second '
+
+// //nextSibiling
+// console.log(itemList.nextSibling)
+// //nextElementSibiling
+// console.log(itemList.nextElementSibling)
+
+// //previousSbiling
+// console.log(itemList.previousSibling)
+// //previousElementSbiling
+// console.log(itemList.previousElementSibling)
+// itemList.previousElementSibling.style.color='green'
+
+//createElement
+
+//create a div
+var newDiv = document.createElement('div')
+//add class
+newDiv.className='hello'
+//add id
+newDiv.id='hello1'
+//add attr
+newDiv.setAttribute('title','Hello div')
+
+//create text node
+var newDivText =document.createTextNode('HEllo word ')
+//add text to div
+newDiv.appendChild(newDivText)
+var container=document.querySelector('header .container')
+var h1 = document.querySelector('header h1')
+console.log(newDiv)
+newDiv.style.fontSize='30px';
+container.insertBefore(newDiv,h1)
+
+ 
+ //insert before item1
+ var newDivTwo= document.createElement('div')
+//add class
+newDivTwo.className='hellotwo'
+//add id
+newDiv.id='hello2'
+//add attr
+newDivTwo.setAttribute('title','Hello divtwo')
+
+var newDivTwoText =document.createTextNode('HEllo word ')
+//add text to div
+newDivTwo.appendChild(newDivTwoText)
+var good=document.querySelector('ul .good')
+var li = document.querySelector('ul li')
+console.log(newDivTwo)
+newDivTwo.style.fontSize='30px';
+good.insertBefore(newDivTwo,li)
+
+
+
+ 
